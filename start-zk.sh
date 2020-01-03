d3484dc930769be6794b4f583300ffb4790d90cf
Christoph Heuwieser <dropdead@hashcat.net>
2020-01-03 20:19:54 +0100

    Update start-zk.sh

--- start-zk.sh
+++ start-zk.sh
@@ -11,6 +11,7 @@ sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 sed -i -r 's|(zookeeper.root.logger=.*)|\1, ROLLINGFILE|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|(zookeeper.log.maxfilesize=.*)|zookeeper.log.maxfilesize=16MB|g' $ZK_HOME/conf/log4j.properties
+export ZOO_LOG4J_PROP="INFO,CONSOLE,ROLLINGFILE"
 
 
 /opt/zookeeper/bin/zkServer.sh start-foreground
