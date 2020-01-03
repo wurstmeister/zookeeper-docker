acddf87dcd73e5379d329b041dadc8b65dbb17a9
Christoph Heuwieser <dropdead@hashcat.net>
2020-01-03 19:20:38 +0100

    added filebeat and consul

--- start-zk.sh
+++ start-zk.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+
+IP=$(hostname -i)
+echo ip=$IP
+
+consul agent -bind=$IP -join $CONSUL_URL -data-dir /opt/consul-data  -config-dir /etc/consul.d &
+
+filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
+
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 
