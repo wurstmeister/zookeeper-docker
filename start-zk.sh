47b729d9889012efeb7a35545cade83e872bac83
Christoph Heuwieser <dropdead@hashcat.net>
2020-01-03 20:02:40 +0100

    Update start-zk.sh

--- start-zk.sh
+++ start-zk.sh
@@ -9,5 +9,6 @@ filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.c
 
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
+sed -i -r 's|(zookeeper.root.logger=.*)|\1, ROLLINGFILE|g' $ZK_HOME/conf/log4j.properties
 
 /opt/zookeeper/bin/zkServer.sh start-foreground
