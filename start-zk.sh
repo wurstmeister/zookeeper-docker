7cfd578830ef793cec8e9a7ace4399200383173d
Christoph Heuwieser <dropdead@hashcat.net>
2020-01-10 14:01:48 +0100

    changed join to -retry-join

--- start-zk.sh
+++ start-zk.sh
@@ -3,7 +3,7 @@
 IP=$(hostname -i)
 echo ip=$IP
 
-consul agent -bind=$IP -join $CONSUL_URL -data-dir /opt/consul-data  -config-dir /etc/consul.d &
+consul agent -bind=$IP -retry-join=$CONSUL_URL -data-dir /opt/consul-data  -config-dir /etc/consul.d &
 
 filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
 
