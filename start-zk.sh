56dd0a35f33cc72fee9916ebdbeb414ae54996b8
Niels Oldenburg <38906791+NielsOldenburg@users.noreply.github.com>
2021-08-31 12:18:14 +0200

    activate prometheus metrics provider

--- start-zk.sh
+++ start-zk.sh
@@ -4,6 +4,8 @@ sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 sed -i -r 's|(zookeeper.root.logger=.*)|\1, ROLLINGFILE|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|(zookeeper.log.maxfilesize=.*)|zookeeper.log.maxfilesize=16MB|g' $ZK_HOME/conf/log4j.properties
+sed -i -r 's|#metricsProvider|metricsProvider|g'  $ZK_HOME/conf/zoo.cfg
+
 export ZOO_LOG4J_PROP="INFO,CONSOLE,ROLLINGFILE"
 
 
